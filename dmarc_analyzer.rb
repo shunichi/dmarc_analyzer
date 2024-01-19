8929d256a708dd0d4422efdb1c7e55eca65a3ac7
Shunichi Ikegami <sike.tm@gmail.com>
2024-01-19 23:11:23 +0900

    statistics by providers

--- dmarc_analyzer.rb
+++ dmarc_analyzer.rb
@@ -18,13 +18,13 @@ class DmarcAnalyzerReport
       rescue Resolv::ResolvError
       end
 
-      # SPF Result は一つだけ
+      # Only one SPF Result
       spf_auth_result = source_record.auth_results.find { |auth_result| auth_result.type == 'spf' }
       if spf_auth_result
         @spf_result = spf_auth_result.result
         @spf_domain = spf_auth_result.domain
       end
-      # DKIM Result は複数ある可能性がある
+      # Multiple DKIM Results are allowed
       dkim_auth_results = source_record.auth_results.select { |auth_result| auth_result.type == 'dkim' }
       dkim_auth_results = dkim_auth_results.partition { |auth_result| auth_result.domain.end_with?(header_from) }.flatten
       if (first_dkim_auth_result = dkim_auth_results.first)
@@ -142,12 +142,16 @@ class DmarcAnalyzerReport
     dmarc_pass_total = 0
     dmarc_or_arc_pass_total = 0
     dmarc_and_arc_fail_total = 0
+    count_by_providers = Hash.new { |h,k| h[k] = { mail_total: 0, dmarc_or_arc_pass_total: 0, dmarc_and_arc_fail_total: 0 } }
     records.each do |record|
       mail_total += record.count
       spf_alignment_pass_total += record.count if record.spf_alignment == 'pass'
       dmarc_pass_total += record.count if record.dmarc_pass
       dmarc_or_arc_pass_total += record.count if record.dmarc_or_arc_pass
       dmarc_and_arc_fail_total += record.count unless record.dmarc_or_arc_pass
+      count_by_providers[record.report_provider][:mail_total] += record.count
+      count_by_providers[record.report_provider][:dmarc_or_arc_pass_total] += record.count if record.dmarc_or_arc_pass
+      count_by_providers[record.report_provider][:dmarc_and_arc_fail_total] += record.count unless record.dmarc_or_arc_pass
     end
     {
       mail_total:,
@@ -155,6 +159,7 @@ class DmarcAnalyzerReport
       dmarc_pass_total:,
       dmarc_or_arc_pass_total:,
       dmarc_and_arc_fail_total:,
+      count_by_providers:,
     }
   end
 
@@ -199,7 +204,9 @@ ARGV.each do |filename|
   analyzer_report.merge!(DmarcAnalyzerReport.new(filename))
 end
 csv_text = analyzer_report.to_csv
-File.write('dmarc_analyzer_result.csv', csv_text)
+out_filename = 'dmarc_analyzer_result.csv'
+File.write(out_filename, csv_text)
+puts "Wrote #{out_filename}"
 
 info = analyzer_report.info
 valid_transfer_count = info[:dmarc_or_arc_pass_total] - info[:spf_alignment_pass_total]
@@ -209,9 +216,20 @@ if info[:mail_total] > 0
   valid_transfer_percent = valid_transfer_count * 100.0 / info[:mail_total]
   invalid_percent = info[:dmarc_and_arc_fail_total] * 100.0 / info[:mail_total]
 end
-puts "期間: #{analyzer_report.begin_at} - #{analyzer_report.end_at}"
-puts "全メール: #{info[:mail_total]}"
-puts "正: #{info[:dmarc_or_arc_pass_total]} (#{format_float valid_percent}%)"
-puts "正(直接): #{info[:spf_alignment_pass_total]} (#{format_float valid_direct_percent}%)"
-puts "正(転送): #{valid_transfer_count} (#{format_float valid_transfer_percent}%)"
-puts "不正: #{info[:dmarc_and_arc_fail_total]} (#{format_float invalid_percent}%)"
+puts "\n--- Statistics ---"
+puts "Time Range: #{analyzer_report.begin_at} - #{analyzer_report.end_at}"
+puts "Total Mails: #{info[:mail_total]}"
+puts "Pass: #{info[:dmarc_or_arc_pass_total]} (#{format_float valid_percent}%)"
+puts "Pass(direct): #{info[:spf_alignment_pass_total]} (#{format_float valid_direct_percent}%)"
+puts "Pass(transfer): #{valid_transfer_count} (#{format_float valid_transfer_percent}%)"
+puts "Fail: #{info[:dmarc_and_arc_fail_total]} (#{format_float invalid_percent}%)"
+
+puts "By providers:"
+info[:count_by_providers].each do |provider, count_hash|
+  total = count_hash[:mail_total]
+  pass_count = count_hash[:dmarc_or_arc_pass_total]
+  fail_count = count_hash[:dmarc_and_arc_fail_total]
+  pass_percent = pass_count * 100.0 / total
+  fail_percent = fail_count * 100.0 / total
+  puts "  #{provider}: #{total} (Pass:#{format_float pass_percent}%, Fail:#{format_float fail_percent}%)"
+end
